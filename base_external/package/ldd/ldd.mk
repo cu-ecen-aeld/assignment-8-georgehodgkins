8f2305e618a4de6a7b7efcc7e6f174f35ccf0569
George <george.hodgkins@colorado.edu>
2021-10-16 11:47:45 -0600

    Add startup script and config fixes

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -4,10 +4,16 @@
 LDD_SITE = 'git@github.com:cu-ecen-aeld/assignment-7-georgehodgkins.git'
 LDD_VERSION = $(shell git ls-remote $(LDD_SITE) master | cut -f1)
 LDD_SITE_METHOD = git
-LDD_GIT_SUBMOUDLES = YES
+LDD_GIT_SUBMODULES = YES
 
-LDD_MODULE_SUBDIRS = 'misc-modules scull'
+LDD_MODULE_SUBDIRS = misc-modules
+LDD_MODULE_SUBDIRS += scull
 LDD_MODULE_MAKE_OPTS = KVERSION=$(LINUX_VERSION_PROBED)
+
+define LDD_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0700 -t $(TARGET_DIR)/usr/bin $(@D)/scull/scull_load $(@D)/scull/scull_unload \
+		$(@D)/misc-modules/module_load $(@D)/misc-modules/module_unload
+endef
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
